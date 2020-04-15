bdfad10267ea825412058ebcb7a30019f1e5712c
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-15 17:32:49 -0400

    Added method that inserts objects

--- js/modules.js
+++ js/modules.js
@@ -1,1 +1,30 @@
-console.log("hello")
+var imageModule = (function () {
+
+    // These are the images we are looping through
+
+    return {
+        carImages: {
+            car1: "http://codingfaculty.com",
+            car2: "http://codingfaculty.com",
+            car3: "http://codingfaculty.com",
+            car4: "http://codingfaculty.com",
+            car5: "http://codingfaculty.com",
+            car6: "http://codingfaculty.com",
+            car7: "http://codingfaculty.com",
+            car8: "http://codingfaculty.com"
+        },
+
+        //This method inserts object
+
+        insertImages: function () {
+            var myArguments = "." + arguments[0]
+            var imagesContainer = document.querySelector(myArguments)
+            var output = "<div>"
+
+            for (var key in this.carImages) {
+                console.log(key)
+                console.log(this.carImages[key])
+            }
+        }
+    }
+})()
